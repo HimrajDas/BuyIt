a8fad3027d69c507f80f306e04205c9ec92a9d6d
nao <iromnao60@gmail.com>
2023-05-22 20:52:24 +0530

    platforms

---
+++ client/client-Ui/src/data.js
@@ -0,0 +1,23 @@
+export const edtechPlatforms = [
+	{
+		id: 1,
+		// img: "https://ibb.co/bBhX1VS"
+		img: "https://i.ibb.co/7gCRhh4/Khan-Academy-Logo-wine.png",
+	},
+	{
+		id: 2,
+		img: "https://i.ibb.co/vP5k1W7/skillshare-icon.png",
+	},
+	{
+		id: 3,
+		img: "https://i.ibb.co/stp2cPr/Pluralsight-Logo-Horizontal-Color-White-Fill.png",
+	},
+	{
+		id: 4,
+		img: "https://i.ibb.co/wM1Vv2s/regular.png",
+	},
+	{
+		id: 5,
+		img: "https://i.ibb.co/t29r9y9/udacity.png",
+	},
+];
