874203873c0af187e4f2dae748581488f976e9ff
HIMRAJ <himrajdas7@gmail.com>
2023-06-14 18:11:19 +0530

    getting 403

--- coursescraper/coursescraper/items.py
+++ coursescraper/coursescraper/items.py
@@ -12,7 +12,7 @@ class CoursescraperItem(scrapy.Item):
     rating = scrapy.Field()
     no_of_ratings = scrapy.Field()
     students_enrolled = scrapy.Field()
-    prerequisites = scrapy.Field()
+    # prerequisites = scrapy.Field()
     reviews = scrapy.Field()
     percentage_of_one_star = scrapy.Field()
     percentage_of_two_star = scrapy.Field()
@@ -20,4 +20,14 @@ class CoursescraperItem(scrapy.Item):
     percentage_of_four_star = scrapy.Field()
     percentage_of_five_star = scrapy.Field()
 
-    pass
+    
+    def __init__(self):
+        super().__init__()
+        self.fields["course_name"] = {}
+
+
+    def __setitem__(self, key, value):
+        if key == "course_name":
+            self.fields[key] = value
+        else:
+            self.fields["course_name"][key] = value
--- coursescraper/coursescraper/spiders/wolf.py
+++ coursescraper/coursescraper/spiders/wolf.py
@@ -1,6 +1,6 @@
 import scrapy
 from urllib.parse import urlencode
-
+from ..items import CoursescraperItem
 
 SCRAPEOPS_API_KEY = "0f1478a6-e418-4393-b9a9-dc3edf2c9689"
 
@@ -37,14 +37,14 @@ class WolfSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        courses = response.css("div.course-card--main-content--2XqiY.course-card--has-price-text--1c0ze")
+        courses = response.css(".course-card--main-content--2XqiY.course-card--has-price-text--1c0ze")
         for course in courses:
             relative_url = course.css("h3 a ::attr(href)").get()
             course_url = "https://udemy.com" + relative_url
             yield scrapy.Request(course_url, callback=self.parse_course_page)
 
 
-        next_page = response.css("div.pagination-module--container--1Dmb0 a:last-child::attr(href)").get()
+        next_page = response.css(".pagination-module--container--1Dmb0 a:last-child::attr(href)").get()
         if next_page is not None:
             next_page_url = "https://udemy.com" + next_page
             yield scrapy.Request(next_page_url, callback=self.parse)
@@ -53,10 +53,22 @@ class WolfSpider(scrapy.Spider):
 
 
     def parse_course_page(self, response):
-        pass
+        course_details = CoursescraperItem()
 
+        course_details["course_name"] = response.css(".clp-lead__title--small::text").get()
+        course_details["rating"] = response.css(".star-rating-module--rating-number--2xeHu::text").get()
+        course_details["no_of_ratings"] = response.css(".star-rating-module--dark-background--3p2UF+ span::text").get()
+        course_details["students_enrolled"] = response.css(".enrollment::text").get()
+        course_details["reviews"] = response.css("div.ud-text-md.show-more-module--content--cjTh0 p::text").getall()
+        course_details["percentage_of_one_star"] = response.css('//*[@id="udemy"]/div[12]/div/div[2]/div[2]/div[1]/div[1]/div/div/div/button[5]/span[3]/span::text').get()
+        course_details["percentage_of_two_star"] = response.css('//*[@id="udemy"]/div[12]/div/div[2]/div[2]/div[1]/div[1]/div/div/div/button[4]/span[3]/span::text').get()
+        course_details["percentage_of_three_star"] = response.css('//*[@id="udemy"]/div[12]/div/div[2]/div[2]/div[1]/div[1]/div/div/div/button[3]/span[3]/span::text').get()
+        course_details["percentage_of_four_star"] = response.css('//*[@id="udemy"]/div[12]/div/div[2]/div[2]/div[1]/div[1]/div/div/div/button[2]/span[3]/span::text').get()
+        course_details["percentage_of_five_star"] = response.css('//*[@id="udemy"]/div[12]/div/div[2]/div[2]/div[1]/div[1]/div/div/div/button[1]/span[3]/span::text').get()
 
+        yield course_details
 
+        
 
 
 
